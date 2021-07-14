14a02677d0525d02a5bc43b203bd2cbb80942152
dimitris <iracleous@gmail.com>
2021-07-14 13:47:32 +0300

    Testing

--- src/main/java/gr/advantage/testing/MainApplication.java
+++ src/main/java/gr/advantage/testing/MainApplication.java
@@ -1,5 +1,9 @@
 package gr.advantage.testing;
 
+import gr.advantage.testing.model.Item;
+import gr.advantage.testing.service.CartService;
+import gr.advantage.testing.service.CartServiceImpl;
+import gr.advantage.testing.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,7 +11,19 @@ public class MainApplication {
     private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
     public static void main(String[] args) {
-      logger.info("The application has been started");
+        PaymentService paymentService = null;
+        CartService cartService = new CartServiceImpl(paymentService);
+
+        cartService.addItem(new Item("Apple", 1, 1));
+        cartService.addItem(new Item("Banana", 2, 5));
+        cartService.addItem(new Item("Apple", 1, 2));
+        cartService.addItem(new Item("Chocolate", 1, 2));
+
+        cartService.getCartItems().forEach(i -> logger.info("{}", i));
+        logger.info("The total price of products is:{}", cartService.getTotalPrice());
+        cartService.removeItem(new Item("Chocolate", 1, 2));
+        cartService.getCartItems().forEach(i -> logger.info("{}", i));
+        logger.info("The total price of products is:{}", cartService.getTotalPrice());
 
 
 
--- src/test/java/gr/advantage/testing/service/CartServiceImplTest.java
+++ src/test/java/gr/advantage/testing/service/CartServiceImplTest.java
@@ -6,24 +6,35 @@ import gr.advantage.testing.model.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static java.time.Duration.ofMillis;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CartServiceImplTest {
     private static final Logger logger = LoggerFactory.getLogger(CartServiceImplTest.class);
 
     private CartServiceImpl cartService;
 
+    @Mock
+    private PaymentService mockedPaymentService;
 
     @BeforeEach
     void initialize() {
-        cartService = new CartServiceImpl(null);
+        cartService = new CartServiceImpl(mockedPaymentService);
         cartService.addItem(new Item("RAM", 120, 2));
     }
 
@@ -49,6 +60,45 @@ class CartServiceImplTest {
     void removeItem() {
         boolean result = cartService.removeItem(new Item("CPU", 300, 1));
         assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Get all cart items")
+    void getCartItems() {
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("RAM", 120, 2));
+        assertIterableEquals(items, cartService.getCartItems());
+    }
+
+
+    @Test
+    @DisplayName("Calculate total cart price")
+    void getTotalPrice() {
+        assertEquals(240, cartService.getTotalPrice());
+    }
+
+
+
+    @Test
+    @DisplayName("Successfull checkout test")
+    void checkout(){
+         when(mockedPaymentService.balance()).thenReturn(300.0);
+        boolean isCompleted = cartService.checkout();
+        int numOfCartItems = cartService.getCartItems().size();
+
+        assertTrue(isCompleted);
+        assertEquals(0, numOfCartItems);
+        verify(mockedPaymentService).withdraw(240);
+    }
+
+
+
+    @Test
+    @DisplayName("Testing the payment service")
+    void checkPaymentService(){
+        when(mockedPaymentService.deposit(any())).thenReturn(true);
+        boolean returnValue = mockedPaymentService.deposit(100.0);
+        assertTrue(returnValue);
     }
 
 }
